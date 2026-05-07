0d1ad717177fd91cf4088e12588d0a809225f4ac
J-J-Xian <jjxzheng60@gmail.com>
2026-05-07 23:40:56 +0800

    CR11402-16, Renewed requirements.

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/johnnyzheng/GitRepo/11402_CS351_Project0
-# Build directory: /Users/johnnyzheng/GitRepo/11402_CS351_Project0/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AllTests "/Users/johnnyzheng/GitRepo/11402_CS351_Project0/build/two_sum_tests")
-set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/johnnyzheng/GitRepo/11402_CS351_Project0/CMakeLists.txt;23;add_test;/Users/johnnyzheng/GitRepo/11402_CS351_Project0/CMakeLists.txt;0;")
